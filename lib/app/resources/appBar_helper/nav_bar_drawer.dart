1bf9e5c090ca688ab52e2252596c48bb6caa3602
MurotjonAbidjanov <murotzonabidzanov@gmail.com>
2023-06-03 15:58:54 +0600

    drawr added

---
+++ lib/app/resources/appBar_helper/nav_bar_drawer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavBarDrawer extends StatelessWidget {
+  const NavBarDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
